a4c95818248b91ecd93c3ca1fc828288bd03457b
Jon Borgonia <jon@gomagames.com>
2017-05-04 15:15:16 -1000

    create Message, get Message with relations

--- api/messages/index.js
+++ api/messages/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const messages = express.Router();
-const { Message } = require('../../models');
+const { Message, User } = require('../../models');
 
 messages.get('/', (req, res) =>
-  Message.all().then( res.json.bind(res) )
+  Message.all({
+    include: [
+      {
+        model: User,
+        as: 'Author'
+      }
+    ]
+  }).then( res.json.bind(res) )
+);
+
+messages.post('/', (req, res) =>
+  Message.create( req.body )
+    .then( res.json.bind(res) )
+    .catch( res.json.bind(res) )
 );
 
 module.exports = messages;
