2be632b2b675863518469de5254d6c4571c66121
Jon Borgonia <jon@gomagames.com>
2017-05-04 14:04:52 -1000

    add associations to User model

--- api/users/index.js
+++ api/users/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const users = express.Router();
-const { User } = require('../../models');
+const { User, Message } = require('../../models');
 
 users.get('/', (req, res) =>
   User.all().then( res.json.bind(res) )
+);
+
+users.get('/:id', (req, res) =>
+  User.findById(req.params.id, {
+    include: [
+      {
+        model: Message,
+        as: 'Author'
+      }
+    ]
+  }).then( res.json.bind(res) )
 );
 
 module.exports = users;
--- models/User.js
+++ models/User.js
@@ -1,6 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Topic, {
+          foreignKey: 'created_by'
+        });
+        User.hasMany(models.Message, {
+          as: 'Author',
+          foreignKey: 'author_id'
+        });
+      }
+    }
   });
 
   return User;
